6ec48e589a26f029fd4bf3228d4329f4a785c519
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-07-01 08:01:29 -0700

    Created bank_account.rb application, included a new BankAccount class.

--- bank_account.rb
+++ bank_account.rb
@@ -1,10 +1,20 @@
 class BankAccount                       # When naming class we use Capital on first character, and "camel case" if multiple world
+    attr_reader :name                   # Allow instance variable to be read
+    
     def initialize(name)                # Gets called when we instantiate the class
         @name = name                    # Creating an instance variable
         @transactions = []              # Create an array to hold the credit and debit transaction
+    end
+
+    def add_transaction(description, amount)                                # Method that accepts 2 arguments
+        @transactions.push(description: description, amount: amount)        # Push to the instance varaible
     end
 end
 
 bank_account = BankAccount.new("Jason")             # Instantiate new instance of BankAccount class
 
 puts bank_account.inspect                           # Inspect our new class object
+
+bank_account.add_transaction("Groceries", 40)
+
+puts bank_account.inspect
